d0cd7bfe33822b854a2d258a7391fa72854c5234
Klayton Lewis <64672448+klaylew@users.noreply.github.com>
2020-11-11 23:55:31 -0800

    Cleaned up JS and added a little detail to the css

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 'use strict';
 
-function getDogImages() {
-  // console.log("Running get dog images")
+let html = ""
+let address = 'https://dog.ceo/api/breeds/image/random/';
+
+function watchForm() {
+  $("form").submit(event => {
+    event.preventDefault();
+    html = address + $("#number").val();
+    console.log(html);
+    generateImages();
+  });
+}
+
+function generateImages() {
   fetch(html)
     .then((res) => res.json())
     .then((resJson) => {
@@ -20,73 +31,8 @@ function getDogImages() {
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-/* function generateGalleryHtml(){
-  let galleryHtml = "";
-  
-  for(let i=0, i < ${responseJson.message).length, i++) {
-    html = 
-    `
-      <div class="gallery">
-          <a target="_blank" href="img_5terre.jpg">
-          <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
-          </a>
-      </div>      
-    `
-  }
-}
-
-function displayResults(responseJson) {
-  console.log(responseJson);
-
-  $('.photos').html(
-
-    // `<img src="${responseJson.message}" class="results-img">`
-    `
-    <div class="gallery">
-        <a target="_blank" href="img_5terre.jpg">
-        <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
-        </a>
-    </div>
-    `
-  )
-  //display the results section
-  $('.results').removeClass('hidden');
-} */
-
-function watchForm() {
-  $("form").submit(event => {
-    event.preventDefault();
-    // html = "";
-    html = address + $("#number").val();
-    console.log(html);
-    // console.log("watchForm ran");
-    getDogImages();
-  });
-}
-
-//function to create API fetch string with entered number of pics
-let html = ""
-let address = 'https://dog.ceo/api/breeds/image/random/';
-
-
-/* function makeFetch(){
-  let address = "https://dog.ceo/api/breeds/image/random/";
-  address = address + $("#number").val();
-  console.log(address);
-  return address;  
-} */
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
-
-
-/** 
- * 
- * need to hold data for number of pictures with a default value of 3
- * 
- * retrieve photo addresses and print to the console
- *  
- * 
- */

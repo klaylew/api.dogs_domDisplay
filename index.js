221dbb73eb08adff5422f035f68236f0c89188a9
Klayton Lewis <64672448+klaylew@users.noreply.github.com>
2020-11-11 23:14:05 -0800

    Full funtionality on selected number of DOM displayed images

--- index.js
+++ index.js
@@ -1,39 +1,80 @@
 'use strict';
 
 function getDogImages() {
-  console.log("Running get dog images")
-  fetch(makeFetch())
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
+  // console.log("Running get dog images")
+  fetch(html)
+    .then((res) => res.json())
+    .then((resJson) => {
+      let output = "";
+      resJson.message.forEach(function(html){
+        console.log(html);
+        output += 
+        `
+        <div class="gallery">
+          <img src=${html} alt="Img" height="400">
+        </div>      
+        `;
+      });
+      document.getElementById('photos').innerHTML = output;
+    })
     .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+/* function generateGalleryHtml(){
+  let galleryHtml = "";
+  
+  for(let i=0, i < ${responseJson.message).length, i++) {
+    html = 
+    `
+      <div class="gallery">
+          <a target="_blank" href="img_5terre.jpg">
+          <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+          </a>
+      </div>      
+    `
+  }
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  /* //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
+
+  $('.photos').html(
+
+    // `<img src="${responseJson.message}" class="results-img">`
+    `
+    <div class="gallery">
+        <a target="_blank" href="img_5terre.jpg">
+        <img src="img_5terre.jpg" alt="Cinque Terre" width="600" height="400">
+        </a>
+    </div>
+    `
   )
   //display the results section
-  $('.results').removeClass('hidden'); */
-}
+  $('.results').removeClass('hidden');
+} */
 
 function watchForm() {
   $("form").submit(event => {
     event.preventDefault();
-    console.log("watchForm ran");
+    // html = "";
+    html = address + $("#number").val();
+    console.log(html);
+    // console.log("watchForm ran");
     getDogImages();
   });
 }
 
 //function to create API fetch string with entered number of pics
-function makeFetch(){
+let html = ""
+let address = 'https://dog.ceo/api/breeds/image/random/';
+
+
+/* function makeFetch(){
   let address = "https://dog.ceo/api/breeds/image/random/";
   address = address + $("#number").val();
   console.log(address);
   return address;  
-}
+} */
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
